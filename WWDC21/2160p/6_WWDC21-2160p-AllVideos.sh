6ec00f9d59b73164224a9c018e64d880be6de94b
Thomas Alvarez <dmthomas@mac.com>
2021-06-06 12:49:57 -0700

    Added stub scripts for WWDC 21

---
+++ WWDC21/2160p/6_WWDC21-2160p-AllVideos.sh
@@ -0,0 +1,24 @@
+# Script to download all the WWDC 21 session videos in the highest 4K video and audio
+# You may have to update ffmpeg before using this script. I needed version 4.3 or higher to successfully download the videos.
+#
+# If you want the lower bitrate audio, do a find/replace of "audio_english_192" with "audio_english_64"
+# If you want lower bitrate or lower resolution video, do a find/replace of "hvc_2160p_16800" with any of the following:
+# "hvc_2160p_11600"
+# "hvc_1440p_8100"
+# "hvc_1080p_5800"
+# "hvc_1080p_4500"
+# "hvc_720p_3400"
+# "hvc_720p_2400"
+# "hvc_540p_1600"
+# "avc_1080p_6000"
+# "avc_720p_4500"
+# "avc_720p_3000"
+# "avc_540p_2000"
+#
+# Test using the below five lines to get the WWDC 20 Monday Wrap-up video because it's short before using all or any of the remaining script.
+#ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2020/10691/2/A92788CB-81ED-4CCF-B6B1-4DD7A1F3E87D/hvc_2160p_16800/prog_index.m3u8 -c copy "Session - 10691 temp.mp4"
+#ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2020/10691/2/A92788CB-81ED-4CCF-B6B1-4DD7A1F3E87D/audio_english_192/prog_index.m3u8 -c copy "Session - 10691 temp.aac"
+#ffmpeg -i "Session - 10691 temp.mp4" -i "Session - 10691 temp.aac" -c copy "Session 10691 - Monday@WWDC.mp4"
+#rm "Session - 10691 temp.mp4"
+#rm "Session - 10691 temp.aac"
+#
