4b4588f62ed12399f95118f9301886e481703e13
Thomas Alvarez <dmthomas@mac.com>
2022-06-07 11:57:15 -0700

    Added subtitles to Monday's videos

--- WWDC22/1440p/1_WWDC22-1440p-Monday.sh
+++ WWDC22/1440p/1_WWDC22-1440p-Monday.sh
@@ -27,30 +27,73 @@
 ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/sdr_hvc_1440p_8100/prog_index.m3u8 -c copy "WWDC 2022 Keynote temp.mp4"
 ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/audio_main_en_2ch_aac_128/prog_index.m3u8 -c copy "WWDC 2022 Keynote temp.aac"
 
+ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/cc/zh/zh.m3u8 -c copy "WWDC 2022 Keynote temp zh.vtt"
+ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/cc/de/de.m3u8 -c copy "WWDC 2022 Keynote temp de.vtt"
+ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/cc/en/en.m3u8 -c copy "WWDC 2022 Keynote temp en.vtt"
+ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/cc/fr/fr.m3u8 -c copy "WWDC 2022 Keynote temp fr.vtt"
+ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/cc/es/es.m3u8 -c copy "WWDC 2022 Keynote temp es.vtt"
+ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/cc/ja/ja.m3u8 -c copy "WWDC 2022 Keynote temp ja.vtt"
+ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/cc/ko/ko.m3u8 -c copy "WWDC 2022 Keynote temp ko.vtt"
+
 #comment out the following three lines if you don't want Dolby Atmos audio
 ffmpeg -i https://events-delivery.apple.com/0205eyyhwbbqexozkwmgccegwnjyrktg/vod_main_KYVtRmtKMbxMmJHvKTwKJVQuxEmzBjPm/audio_main_en_16ch_atmos_640/prog_index.m3u8 -c copy "WWDC 2022 Keynote temp atmos.mp4"
-ffmpeg -i "WWDC 2022 Keynote temp.mp4" -i "WWDC 2022 Keynote temp atmos.mp4" -i "WWDC 2022 Keynote temp.aac" -map 0 -map 1 -map 2 -metadata:s:a:0 title="Dolby Atmos" -metadata:s:a:0 language=eng -metadata:s:a:1 title="Stereo" -metadata:s:a:1 language=eng -c copy "Apple WWDC 2022 Keynote Address (1440p).mp4"
+ffmpeg -i "WWDC 2022 Keynote temp.mp4" -i "WWDC 2022 Keynote temp atmos.mp4" -i "WWDC 2022 Keynote temp.aac" -i "WWDC 2022 Keynote temp zh.vtt" -i "WWDC 2022 Keynote temp de.vtt" -i "WWDC 2022 Keynote temp en.vtt" -i "WWDC 2022 Keynote temp fr.vtt" -i "WWDC 2022 Keynote temp es.vtt" -i "WWDC 2022 Keynote temp ja.vtt" -i "WWDC 2022 Keynote temp ko.vtt" -map 0:v -map 1:a -map 2:a -metadata:s:a:0 title="Dolby Atmos" -metadata:s:a:0 language=eng -metadata:s:a:1 title="Stereo" -metadata:s:a:1 language=eng -acodec copy -vcodec copy -map 3 -c:s:0 mov_text -metadata:s:s:0 language=zho -map 4 -c:s:1 mov_text -metadata:s:s:1 language=deu -map 5 -c:s:2 mov_text -metadata:s:s:2 language=eng -map 6 -c:s:3 mov_text -metadata:s:s:3 language=fra -map 7 -c:s:4 mov_text -metadata:s:s:4 language=spa -map 8 -c:s:5 mov_text -metadata:s:s:5 language=jpn -map 9 -c:s:6 mov_text -metadata:s:s:6 language=kor "Apple WWDC 2022 Keynote Address (1440p).mp4"
 rm "WWDC 2022 Keynote temp atmos.mp4"
 
 #uncomment the following line if you didn't want Dolby Atmos audio
-#ffmpeg -i "WWDC 2022 Keynote temp.mp4" -i "WWDC 2022 Keynote temp.aac" -c copy "Apple WWDC 2022 Keynote Address (1080p).mp4"
+#ffmpeg -i "WWDC 2022 Keynote temp.mp4" -i "WWDC 2022 Keynote temp.aac" -c copy "Apple WWDC 2022 Keynote Address (1440p).mp4"
 
 rm "WWDC 2022 Keynote temp.mp4"
 rm "WWDC 2022 Keynote temp.aac"
+rm "WWDC 2022 Keynote temp zh.vtt"
+rm "WWDC 2022 Keynote temp de.vtt"
+rm "WWDC 2022 Keynote temp en.vtt"
+rm "WWDC 2022 Keynote temp fr.vtt"
+rm "WWDC 2022 Keynote temp es.vtt"
+rm "WWDC 2022 Keynote temp ja.vtt"
+rm "WWDC 2022 Keynote temp ko.vtt"
 
 # WWDC22 Platforms State of the Union
 ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/102/3/29AB52D7-09CF-4A22-8C26-84AEB4A90097/cmaf/hvc/1440p_8100/hvc_1440p_8100.m3u8 -c copy "WWDC 2022 Platforms State of the Union temp.mp4"
 ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/102/3/29AB52D7-09CF-4A22-8C26-84AEB4A90097/cmaf/aac/lc_192/aac_lc_192.m3u8 -c copy "WWDC 2022 Platforms State of the Union temp.aac"
-ffmpeg -i "WWDC 2022 Platforms State of the Union temp.mp4" -i "WWDC 2022 Platforms State of the Union temp.aac" -c copy "WWDC 2022 Platforms State of the Union (1440p).mp4"
+
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/102/3/29AB52D7-09CF-4A22-8C26-84AEB4A90097/subtitles/eng/prog_index.m3u8 -c copy "WWDC 2022 Platforms State of the Union temp en.vtt"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/102/3/29AB52D7-09CF-4A22-8C26-84AEB4A90097/subtitles/zho/prog_index.m3u8 -c copy "WWDC 2022 Platforms State of the Union temp zh.vtt"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/102/3/29AB52D7-09CF-4A22-8C26-84AEB4A90097/subtitles/jpn/prog_index.m3u8 -c copy "WWDC 2022 Platforms State of the Union temp ja.vtt"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/102/3/29AB52D7-09CF-4A22-8C26-84AEB4A90097/subtitles/kor/prog_index.m3u8 -c copy "WWDC 2022 Platforms State of the Union temp ko.vtt"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/102/3/29AB52D7-09CF-4A22-8C26-84AEB4A90097/subtitles/fra/prog_index.m3u8 -c copy "WWDC 2022 Platforms State of the Union temp fr.vtt"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/102/3/29AB52D7-09CF-4A22-8C26-84AEB4A90097/subtitles/deu/prog_index.m3u8 -c copy "WWDC 2022 Platforms State of the Union temp de.vtt"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/102/3/29AB52D7-09CF-4A22-8C26-84AEB4A90097/subtitles/spa/prog_index.m3u8 -c copy "WWDC 2022 Platforms State of the Union temp es.vtt"
+
+ffmpeg -i "WWDC 2022 Platforms State of the Union temp.mp4" -i "WWDC 2022 Platforms State of the Union temp.aac" -i "WWDC 2022 Platforms State of the Union temp en.vtt" -i "WWDC 2022 Platforms State of the Union temp zh.vtt" -i "WWDC 2022 Platforms State of the Union temp ja.vtt" -i "WWDC 2022 Platforms State of the Union temp ko.vtt" -i "WWDC 2022 Platforms State of the Union temp fr.vtt" -i "WWDC 2022 Platforms State of the Union temp de.vtt" -i "WWDC 2022 Platforms State of the Union temp es.vtt" -map 0:v -map 1:a -acodec copy -vcodec copy -map 2 -c:s:0 mov_text -metadata:s:s:0 language=eng -map 3 -c:s:1 mov_text -metadata:s:s:1 language=zho -map 4 -c:s:2 mov_text -metadata:s:s:2 language=jpn -map 5 -c:s:3 mov_text -metadata:s:s:3 language=kor -map 6 -c:s:4 mov_text -metadata:s:s:4 language=fra -map 7 -c:s:5 mov_text -metadata:s:s:5 language=deu -map 8 -c:s:6 mov_text -metadata:s:s:6 language=spa "Apple WWDC 2022 Platforms State of the Union (1440p).mp4"
+
 rm "WWDC 2022 Platforms State of the Union temp.mp4"
 rm "WWDC 2022 Platforms State of the Union temp.aac"
+rm "WWDC 2022 Platforms State of the Union temp en.vtt"
+rm "WWDC 2022 Platforms State of the Union temp zh.vtt"
+rm "WWDC 2022 Platforms State of the Union temp ja.vtt"
+rm "WWDC 2022 Platforms State of the Union temp ko.vtt"
+rm "WWDC 2022 Platforms State of the Union temp fr.vtt"
+rm "WWDC 2022 Platforms State of the Union temp de.vtt"
+rm "WWDC 2022 Platforms State of the Union temp es.vtt"
 
 # 2022 Apple Design Awards
-ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/103/3/AFC3701A-AC3C-4F5D-9AB3-1CDE42CB5763/cmaf/hvc/1440p_8100/hvc_1440p_8100.m3u8 -c copy "2022 Apple Design Awards temp.mp4"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/103/3/AFC3701A-AC3C-4F5D-9AB3-1CDE42CB5763/cmaf/hvc/1440p_8100/hvc_1440p_8100.m3u88 -c copy "2022 Apple Design Awards temp.mp4"
 ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/103/3/AFC3701A-AC3C-4F5D-9AB3-1CDE42CB5763/cmaf/aac/lc_192/aac_lc_192.m3u8 -c copy "2022 Apple Design Awards temp.aac"
-ffmpeg -i "2022 Apple Design Awards temp.mp4" -i "2022 Apple Design Awards temp.aac" -c copy "2022 Apple Design Awards (1440p).mp4"
+
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/103/3/AFC3701A-AC3C-4F5D-9AB3-1CDE42CB5763/subtitles/eng/prog_index.m3u8 -c copy "2022 Apple Design Awards temp en.vtt"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/103/3/AFC3701A-AC3C-4F5D-9AB3-1CDE42CB5763/subtitles/kor/prog_index.m3u8 -c copy "2022 Apple Design Awards temp ko.vtt"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/103/3/AFC3701A-AC3C-4F5D-9AB3-1CDE42CB5763/subtitles/zho/prog_index.m3u8 -c copy "2022 Apple Design Awards temp zh.vtt"
+ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/103/3/AFC3701A-AC3C-4F5D-9AB3-1CDE42CB5763/subtitles/jpn/prog_index.m3u8 -c copy "2022 Apple Design Awards temp ja.vtt"
+
+ffmpeg -i "2022 Apple Design Awards temp.mp4" -i "2022 Apple Design Awards temp.aac" -i "2022 Apple Design Awards temp en.vtt" -i "2022 Apple Design Awards temp ko.vtt" -i "2022 Apple Design Awards temp zh.vtt" -i "2022 Apple Design Awards temp ja.vtt" -map 0:v -map 1:a -acodec copy -vcodec copy -map 2 -c:s:0 mov_text -metadata:s:s:0 language=eng -map 3 -c:s:1 mov_text -metadata:s:s:1 language=kor -map 4 -c:s:2 mov_text -metadata:s:s:2 language=zho -map 5 -c:s:3 mov_text -metadata:s:s:3 language=jpn "2022 Apple Design Awards (1440p).mp4"
+
 rm "2022 Apple Design Awards temp.mp4"
 rm "2022 Apple Design Awards temp.aac"
+rm "2022 Apple Design Awards temp en.vtt"
+rm "2022 Apple Design Awards temp ko.vtt"
+rm "2022 Apple Design Awards temp zh.vtt"
+rm "2022 Apple Design Awards temp ja.vtt"
 
 # Session 110929 - Monday@WWDC22
 ffmpeg -i https://devstreaming-cdn.apple.com/videos/wwdc/2022/110929/1/E9996C71-5D71-46C7-BC47-4A26302DA7D6/cmaf/hvc/1440p_8100/hvc_1440p_8100.m3u8 -c copy "Session - 110929 temp.mp4"
